6f691bdc763410ca173fb4b7048cf9bc24ac6e4f
ludik <bakhanetssimon@gmail.com>
2026-01-04 14:02:04 +0700

    Initial Commit

---
+++ Poker Value Assistant/Poker_Value_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_Value_AssistantApp.swift
+//  Poker Value Assistant
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Poker_Value_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Poker Value AssistantTests/Poker_Value_AssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  Poker_Value_AssistantTests.swift
+//  Poker Value AssistantTests
+//
+//  Created by Simon Bakhanets on 04.01.2026.
+//
+
+import Testing
+
+struct Poker_Value_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
